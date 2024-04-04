5dd6bf17c92f7148b82ecb852731a938fc1c20cf
Eric Robsky Huntley <ericrobskyhuntley@gmail.com>
2024-04-04 19:16:29 -0400

    replace epoch with human-readable datetime

--- update-from-remote.sh
+++ update-from-remote.sh
@@ -6,79 +6,82 @@
 # been updated, downloads the updated file and moves the outdated
 # local copy to an archive folder.
 
-URL=''
+url=''
 
 while getopts "u:" flag; do
  case $flag in
    u)
    # URL of remote file.
-   URL="$OPTARG"
+   url="$OPTARG"
    ;;
  esac
 done
 
-if [[ -z "$URL" ]]; then
+if [[ -z "$url" ]]; then
     echo "You must provide a remote file URL."
     exit 1
 fi
 
 echo "Grabbing modified dates from remote file using HEAD request."
 
-FILENAME=$(basename "$URL")
-LOG_CSV="log.csv"
+filename=$(basename "$url")
+log_csv="log.csv"
 
 # Create CSV if it doesn't exist.
-if [ ! -f "$LOG_CSV" ]; then
-    UPDATE=1
-    FIRSTRUN=1
-    echo "$LOG_CSV does not exist. Creating...";
-    echo "url,date,date_ep,mod,mod_ep,update" >> $LOG_CSV
+if [ ! -f "$log_csv" ]; then
+    update=1
+    echo "$log_csv does not exist. Creating...";
+    echo "url,date,mod,update" >> $log_csv
 fi
 
-CURL=$(curl -s -v -X HEAD "$URL" 2>&1)
+curl=$(curl -s -v -X HEAD "$url" 2>&1)
 
 # Break and log on 404 error.
-if [[ $CURL = *'404 Not Found'* ]]; then
+if [[ $curl = *'404 Not Found'* ]]; then
     echo '404 Error! Check your URL.'
     exit 1
 fi
 
-DATE=$("$CURL" 2>&1 | sed -n -e 's/< Date\: .*\, //p'  | tr -d "\t\n\r")
-MOD=$("$CURL" 2>&1 | sed -n -e 's/< Last-Modified\: .*\, //p' | tr -d "\t\n\r")
+date=$("$curl" 2>&1 | sed -n -e 's/< Date\: .*\, //p'  | tr -d "\t\n\r")
+mod=$("$curl" 2>&1 | sed -n -e 's/< Last-Modified\: .*\, //p' | tr -d "\t\n\r")
+echo "$date"
 
 if [ "$(uname)" == "Darwin" ]; then
-    DATE_EP=$(date -j -f "%d %b %Y %H:%M:%S %Z" "$DATE" +%s)
-    MOD_EP=$(date -j -f "%d %b %Y %H:%M:%S %Z" "$MOD" +%s)
+    date_ep=$(date -j -f "%d %b %Y %H:%M:%S %Z" "$date" "+%Y%m%d-%H%M%S")
+    mod_ep=$(date -j -f "%d %b %Y %H:%M:%S %Z" "$mod" "+%Y%m%d-%H%M%S")
 else
-    DATE_EP=$(date -d "$DATE" "+%s")
-    MOD_EP=$(date -d "$MOD" "+%s")
+    date_ep=$(date -d "$date" "%Y%m%d-%H%M%S")
+    mod_ep=$(date -d "$mod" "%Y%m%d-%H%M%S")
 fi
 
-CSV_LINES=$(wc -l < "$LOG_CSV" | xargs)
-MOD_LAST_EP=$(tail -1 "$LOG_CSV" | awk -F',' '{print $5}')
+csv_lines=$(wc -l < "$log_csv" | xargs)
+mod_last_ep=$(tail -1 "$log_csv" | awk -F',' '{print $3}')
 
-if [ "$CSV_LINES" -eq 1 ]; then
-    echo "No prior successful runs. Downloading file, modified on $MOD."
-    UPDATE=1
-elif [ "$MOD_EP" -gt "$MOD_LAST_EP" ]; then
-    echo "There was an update to the file on $MOD."
-    UPDATE=1
+if [ "$csv_lines" -eq 1 ]; then
+    echo "No prior successful runs. Downloading file, modified on $mod."
+    update=1
+elif [ "$mod_ep" -gt "$mod_last_ep" ]; then
+    echo "There was an update to the file on $mod."
+    update=1
 else
     echo "No update to the file since last run."
-    UPDATE=0
+    update=0
 fi
 
-if [ "$UPDATE" -eq 1 ]; then
+if [ "$update" -eq 1 ]; then
     echo "Downloading updated file."
     { 
-        curl -o "$MOD_EP-$FILENAME" "$URL"
+        filename_only="${filename%.*}"
+        extension="${filename##*.}"
+
+        curl -o "${filename_only}_${mod_ep}.${extension}" "$url"
         # Move outdated file to archive folder.
-        if [ -f "$MOD_LAST_EP-$FILENAME" ]; then
+        if [ -f "${filename_only}_${mod_ep}.${extension}" ]; then
             mkdir -p 'archive'
-            mv "$MOD_LAST_EP-$FILENAME" "archive/$MOD_LAST_EP-$FILENAME"
+            mv "${filename_only}_${mod_ep}.${extension}" "archive/${filename_only}_${mod_ep}.${extension}"
         fi
     }
 fi
 
-echo "$URL,$DATE,$DATE_EP,$MOD,$MOD_EP,$UPDATE" >> $LOG_CSV
+echo "$url,$date_ep,$mod_ep,$update" >> $log_csv
 
